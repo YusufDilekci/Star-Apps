1aa2db2dca1fb877b59431742538f79021b85a82
YusufDilekci <96924500+YusufDilekci@users.noreply.github.com>
2022-08-26 15:46:47 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+/*
+ Increasing Triangle Pattern
+    int n=5;
+    for(int y=1 ; y<=n ;y++){
+        for(int x=1; x<=y; x++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+   #############################################3
+
+   Decreasing Triangle Pattern
+    int n;
+    for(int y=1; y<=n ; y++){
+        for(int x=y; x<=n; x++){
+                printf("*");
+        }
+        printf("\n");
+    }
+
+
+Right Sided Triangle
+ int n=5;
+ for(int y=1; y<=n ; y++){
+    for(int x=1; x<=y; x++){
+        printf(" ");
+    }
+    for(int x=y; x<=n ; x++){
+        printf("*");
+    }
+    printf("\n");
+ }
+
+
+Hill Pattern
+int n=5;
+for(int y=1; y<=n; y++){
+    for(int x=y ; x<=n; x++){
+        printf(" ");
+    }
+    for(int x=1; x<=y ;x++){
+        printf("*");
+    }
+    for(int x=1; x<y ;x++){
+        printf("*");
+    }
+
+
+    printf("\n");
+}
+
+Reverse Hill Pattern
+int n=5;
+for(int y=1; y<=n; y++){
+    for(int x=1; x<y; x++){
+        printf(" ");
+    }
+    for(int x=y; x<=n; x++){
+        printf("*");
+    }
+    for(int x=y ; x<n; x++){
+        printf("*");
+    }
+    printf("\n");
+}
+
+
+Diamond Pattern
+int n=5;
+for(int y=1; y<n; y++){
+    for(int x=y ; x<=n; x++){
+        printf(" ");
+    }
+    for(int x=1; x<=y ;x++){
+        printf("*");
+    }
+    for(int x=1; x<y ;x++){
+        printf("*");
+    }
+    printf("\n");
+}
+for(int y=1; y<=n; y++){
+    for(int x=1; x<=y; x++){
+        printf(" ");
+    }
+    for(int x=y; x<=n; x++){
+        printf("*");
+    }
+    for(int x=y ; x<n; x++){
+        printf("*");
+    }
+    printf("\n");
+}
+
+
+Butterfly Pattern
+int n=3;
+for(int y=1; y<=n; y++){
+    for(int x=1; x<=y; x++){
+        printf("*");
+    }
+    for(int x=y; x<=n; x++){
+        printf(" ");
+    }
+    for(int x=y; x<n; x++){
+        printf(" ");
+
+    }
+    for(int x=1; x<=y; x++){
+        printf("*");
+    }
+    printf("\n");
+}
+for(int x=1; x<=2*n+1; x++){
+    printf("*");
+}
+printf("\n");
+
+for(int y=1; y<=n ; y++){
+        for(int x=y; x<=n; x++){
+            printf("*");
+        }
+        for(int x=1; x<=y; x++){
+            printf(" ");
+        }
+        for(int x=1; x<y; x++){
+            printf(" ");
+        }
+        for(int x=y; x<=n; x++){
+            printf("*");
+        }
+        printf("\n");
+}
+
+*/
+    return 0;
+}
